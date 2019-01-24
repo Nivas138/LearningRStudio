8b98ee8f44b10f0dd6807886bc354a33dcdcaffa
Nivas Ganesan <38051335+Nivas138@users.noreply.github.com>
2019-01-24 22:38:12 +0530

    Greatest Of Three numbers

---
+++ task_6_gratestofthree.r
@@ -0,0 +1,18 @@
+greatest<-function(a,b,c){
+  if(a>b){
+    if(a>c){
+      print(a)
+    }
+  }
+  else{
+    if(b>c){
+      print(b)
+    }
+    else{
+      print(c)
+    }
+  }
+}
+greatest(55,440,555)
+
+
